2a4e202e7d7126b23efef1316915c7022d13deef
Bolun Zhang <13580850696@163.com>
2022-09-19 17:48:47 +0100

    Third Commit

---
+++ stdr_gmapping/maps/saveMap.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Copyright: 2016-2018 www.corvin.cn
+# Author: corvin
+# Description: bash file to save stdr_gmapping's map
+# History:
+#  20180524: initial this bash file.
+rosrun map_server map_server -f mamap
+exit 0
